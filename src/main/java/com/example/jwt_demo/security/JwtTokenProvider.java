d9b5d2df4d33ed5e7638225ec7746d57d39b384e
Gumraze-git <galaxydh4110@gmail.com>
2025-06-22 22:25:46 +0900

    fix(security): @Value 애너테이션 import 구문 수정

--- src/main/java/com/example/jwt_demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/jwt_demo/security/JwtTokenProvider.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
